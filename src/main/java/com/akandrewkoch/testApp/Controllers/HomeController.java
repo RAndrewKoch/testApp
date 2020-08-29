11cfb863414a48a0a8edad689eadc011bd943775
Andy Koch <akandrewkoch@gmail.com>
2020-08-29 16:56:44 -0500

    Initial commit

---
+++ src/main/java/com/akandrewkoch/testApp/Controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.akandrewkoch.testApp.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String displayHomePage(){
+        return "index.html";
+    }
+}
