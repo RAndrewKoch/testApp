1f7ed7d5ebf56c2bd021f3bf9ccf1e73c0c493b0
Andy Koch <akandrewkoch@gmail.com>
2020-09-04 14:22:17 -0500

    -added "Thing" entity

---
+++ src/main/java/com/akandrewkoch/testApp/Models/Thing.java
@@ -0,0 +1,44 @@
+package com.akandrewkoch.testApp.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Thing {
+
+    @Id
+    private Integer thingNumber;
+
+    Thing (){}
+
+    Thing (Integer thingNumber){
+        this.thingNumber=thingNumber;
+    }
+
+    public Integer getThingNumber() {
+        return thingNumber;
+    }
+
+    public void setThingNumber(Integer thingNumber) {
+        this.thingNumber = thingNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Thing)) return false;
+        Thing thing = (Thing) o;
+        return Objects.equals(getThingNumber(), thing.getThingNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getThingNumber());
+    }
+
+    @Override
+    public String toString() {
+        return "Thing{" + "thingNumber=" + thingNumber + '}';
+    }
+}
